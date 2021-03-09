3e6e4739d90c550d02e9106f55d56e8fc4bd3718
jacckmark@gmail.com <jacckmark@gmail.com>
2021-03-09 08:04:31 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <amplify-authenticator>
+    <div>
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+      <amplify-sign-out></amplify-sign-out>
+    </div>
+  </amplify-authenticator>
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld
+    }
+  }
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <img src="../assets/200.gif" alt="" />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .hello {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 30px;
+  }
+  .hello h1 {
+    width: 400px;
+  }
+  .hello img {
+    width: 400px;
+  }
+</style>
