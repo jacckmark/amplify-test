9279fa698a6205a1e6605ae02ecca1883af230d4
jacckmark@gmail.com <jacckmark@gmail.com>
2021-03-09 11:06:10 +0100

    changed auth aws amplify configuration

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from "vue";
 import App from "./App.vue";
-import Amplify from "aws-amplify";
+import Amplify, { Auth } from "aws-amplify";
 import awsconfig from "./aws-exports";
 import "@aws-amplify/ui-vue";
 import router from "./router";
 
 Amplify.configure(awsconfig);
+Auth.configure(awsconfig);
 Vue.config.productionTip = false;
 
 new Vue({
